2bc0ce8e0d935d95f0f21df802ba1938663cd083
sviridov-andrey <chosenfew@yandex.ru>
2023-03-28 01:37:33 +0300

    Добавлена функция filtered_executed_operation()

---
+++ utils.py
@@ -0,0 +1,19 @@
+import json
+
+
+def filtered_executed_operation(filename):
+    """фильтрует исполненные (executed) банковские операции из файла operations.json"""
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+        filtered_executed = [item for item in data if item.get("state", None) == "EXECUTED"]
+        return filtered_executed
+
+
+
+filtered_executed_operation("operations.json")
+
+
+
+
