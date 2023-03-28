1a530b1f8ff5d3bbbb1b55321df2be6704b1ac6c
sviridov-andrey <chosenfew@yandex.ru>
2023-03-28 21:47:12 +0300

    Изменены названия функций. Стало filtered_executed_transactions() и sorted_data_executed_transactions()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from utils import filtered_executed_operation, sorted_data_executed_operation
+from utils import filtered_executed_transactions, sorted_data_executed_transactions
 
 if __name__ == '__main__':
 
-    filtered_executed = filtered_executed_operation("operations.json")
-    sorted_data_executed_operation(filtered_executed)
+    filtered_executed = filtered_executed_transactions("operations.json")
+    sorted_data_executed_transactions(filtered_executed)
