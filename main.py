cf537f5066e5a08c36fc84b73eda766b04d79980
sviridov-andrey <chosenfew@yandex.ru>
2023-03-30 00:55:17 +0300

    исправлена случайная опечатка. программа работает корректно

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
 
     filtered_executed = filtered_executed_transactions("operations.json")
     sorted_data = sorted_data_executed_transactions(filtered_executed)
-    last_five_transactions = add_last_five_transactions(sorted_data)до
+    last_five_transactions = add_last_five_transactions(sorted_data)
     output_last_five_transactions = output_last_five_transactions(last_five_transactions)
